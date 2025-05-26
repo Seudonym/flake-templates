5658b22de9500502f760b33d97d6f07ae80acca6
Seudonym <wk170179@gmail.com>
2025-05-26 13:13:11 +0530

    update root flake

--- flake.nix
+++ flake.nix
@@ -8,6 +8,11 @@
         description = "A simple Rust application template";
       };
 
+      c-cpp = {
+        path = ./c-cpp;
+        description = "A simple C/C++ application template";
+      };
+
       defaultTemplate = self.templates.rust;
     };
   };
